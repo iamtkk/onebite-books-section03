ec1f7da1a364138f812cc4a70ce2f3570dcde7d5
김태관 <iamtk@neowiz.com>
2024-11-04 22:12:41 +0900

    ui

--- src/app/search/page.tsx
+++
@@ -1,9 +0,0 @@
-const Page = async ({
-  searchParams,
-}: {
-  searchParams: Promise<{ q: string }>;
-}) => {
-  const { q } = await searchParams;
-  return <div>Search 페이지 : {q}</div>;
-};
-export default Page;
